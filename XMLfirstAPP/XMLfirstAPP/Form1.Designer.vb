f9fe2d524bf0fb39c9f373efe1c5cb200d0443a2
rtqayyum <rtqayyum@gmail.com>
2014-05-06 13:33:10 +0500

    XML File Reading
    It will simple read XML in VB.Net and show given data in a form with the
    facility of navigation

---
+++ XMLfirstAPP/XMLfirstAPP/Form1.Designer.vb
@@ -0,0 +1,159 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtID = New System.Windows.Forms.TextBox()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtGPA = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnLast = New System.Windows.Forms.Button()
+        Me.btnFirst = New System.Windows.Forms.Button()
+        Me.btnPrevious = New System.Windows.Forms.Button()
+        Me.btnNext = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(33, 30)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(58, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Student ID"
+        '
+        'txtID
+        '
+        Me.txtID.Location = New System.Drawing.Point(119, 27)
+        Me.txtID.Name = "txtID"
+        Me.txtID.Size = New System.Drawing.Size(356, 20)
+        Me.txtID.TabIndex = 1
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(119, 78)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(356, 20)
+        Me.txtName.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(33, 81)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(75, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Student Name"
+        '
+        'txtGPA
+        '
+        Me.txtGPA.Location = New System.Drawing.Point(119, 129)
+        Me.txtGPA.Name = "txtGPA"
+        Me.txtGPA.Size = New System.Drawing.Size(356, 20)
+        Me.txtGPA.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(33, 132)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(69, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Student GPA"
+        '
+        'btnLast
+        '
+        Me.btnLast.Location = New System.Drawing.Point(389, 167)
+        Me.btnLast.Name = "btnLast"
+        Me.btnLast.Size = New System.Drawing.Size(86, 57)
+        Me.btnLast.TabIndex = 6
+        Me.btnLast.Text = "Last >>"
+        Me.btnLast.UseVisualStyleBackColor = True
+        '
+        'btnFirst
+        '
+        Me.btnFirst.Location = New System.Drawing.Point(119, 167)
+        Me.btnFirst.Name = "btnFirst"
+        Me.btnFirst.Size = New System.Drawing.Size(86, 57)
+        Me.btnFirst.TabIndex = 7
+        Me.btnFirst.Text = "<< First"
+        Me.btnFirst.UseVisualStyleBackColor = True
+        '
+        'btnPrevious
+        '
+        Me.btnPrevious.Location = New System.Drawing.Point(209, 167)
+        Me.btnPrevious.Name = "btnPrevious"
+        Me.btnPrevious.Size = New System.Drawing.Size(86, 57)
+        Me.btnPrevious.TabIndex = 8
+        Me.btnPrevious.Text = "< Previous"
+        Me.btnPrevious.UseVisualStyleBackColor = True
+        '
+        'btnNext
+        '
+        Me.btnNext.Location = New System.Drawing.Point(299, 167)
+        Me.btnNext.Name = "btnNext"
+        Me.btnNext.Size = New System.Drawing.Size(86, 57)
+        Me.btnNext.TabIndex = 9
+        Me.btnNext.Text = "Next >"
+        Me.btnNext.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(528, 254)
+        Me.Controls.Add(Me.btnNext)
+        Me.Controls.Add(Me.btnPrevious)
+        Me.Controls.Add(Me.btnFirst)
+        Me.Controls.Add(Me.btnLast)
+        Me.Controls.Add(Me.txtGPA)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtID)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtID As System.Windows.Forms.TextBox
+    Friend WithEvents txtName As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtGPA As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents btnLast As System.Windows.Forms.Button
+    Friend WithEvents btnFirst As System.Windows.Forms.Button
+    Friend WithEvents btnPrevious As System.Windows.Forms.Button
+    Friend WithEvents btnNext As System.Windows.Forms.Button
+
+End Class
---
+++ XMLfirstAPP/XMLfirstAPP/Form1.vb
@@ -0,0 +1,76 @@
+﻿Public Class Form1
+    Private xmlDocument As New System.Xml.XmlDocument
+    Private ids As System.Xml.XmlNodeList
+    Private names As System.Xml.XmlNodeList
+    Private gpas As System.Xml.XmlNodeList
+    Private id As System.Xml.XmlNode
+    Private nm As System.Xml.XmlNode
+    Private gpa As System.Xml.XmlNode
+    Private count As Integer = 0
+    Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+       
+        OpenFileDialog1.ShowDialog()
+
+        xmlDocument.Load(OpenFileDialog1.FileName)
+        ids = xmlDocument.GetElementsByTagName("id")
+        names = xmlDocument.GetElementsByTagName("name")
+        gpas = xmlDocument.GetElementsByTagName("gpa")
+
+        id = ids.ItemOf(count)
+        nm = names.ItemOf(count)
+        gpa = gpas.ItemOf(count)
+        txtID.Text = id.InnerText
+        txtName.Text = nm.InnerText
+        txtGPA.Text = gpa.InnerText
+    End Sub
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        
+    End Sub
+
+    Private Sub btnFirst_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnFirst.Click
+        count = 0
+        id = ids.ItemOf(count)
+        nm = names.ItemOf(count)
+        gpa = gpas.ItemOf(count)
+        txtID.Text = id.InnerText
+        txtName.Text = nm.InnerText
+        txtGPA.Text = gpa.InnerText
+    End Sub
+
+    Private Sub btnLast_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnLast.Click
+        count = ids.Count - 1
+        id = ids.ItemOf(count)
+        nm = names.ItemOf(count)
+        gpa = gpas.ItemOf(count)
+        txtID.Text = id.InnerText
+        txtName.Text = nm.InnerText
+        txtGPA.Text = gpa.InnerText
+    End Sub
+
+    Private Sub btnPrevious_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnPrevious.Click
+        count = count - 1
+        If count >= 0 Then
+            id = ids.ItemOf(count)
+            nm = names.ItemOf(count)
+            gpa = gpas.ItemOf(count)
+            txtID.Text = id.InnerText
+            txtName.Text = nm.InnerText
+            txtGPA.Text = gpa.InnerText
+        End If
+    End Sub
+
+    Private Sub btnNext_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnNext.Click
+        count = count + 1
+        If count < ids.Count Then
+            id = ids.ItemOf(count)
+            nm = names.ItemOf(count)
+            gpa = gpas.ItemOf(count)
+            txtID.Text = id.InnerText
+            txtName.Text = nm.InnerText
+            txtGPA.Text = gpa.InnerText
+        End If
+    End Sub
+End Class
